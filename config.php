12c7af2266aa568240650626243b53288b6dd263
unknown <indraalfiansyah37@gmail.com>
2024-04-24 20:58:07 +0700

    kasirr

--- config.php
+++ config.php
@@ -1,15 +1,21 @@
 <?php
 /*
- keterangan : untuk login aplikasi dengan username : admin dan password : 12345
-*/
+  | Source Code Aplikasi Penjualan produk Kasir dengan PHP & MYSQL
+  |
+  | keterangan : untuk login aplikasi dengan username : admin dan password : 12345
+  | 
+  | 
+  | 
+ */
+
 date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 	// sesuaikan dengan server anda
 	$host 	= 'localhost'; // host server
-	$user 	= 'root';  // username server
-	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
-	$dbname = 'kasirr'; // nama database anda
+	$user 	= 'id21084691_tokohmesran';  // username server
+	$pass 	= 'kasirH.mesran0801'; // password server, kalau pakai xampp kosongin saja
+	$dbname = 'id21084691_kasir'; // nama database anda
 	
 	try{
 		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
