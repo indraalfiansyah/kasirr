664b9955eb38d216b89ac7747c98b0896cca10e2
unknown <indraalfiansyah37@gmail.com>
2024-04-24 17:31:53 +0700

    kasirr

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
-
+/*
+ keterangan : untuk login aplikasi dengan username : admin dan password : 12345
+*/
 date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 	// sesuaikan dengan server anda
 	$host 	= 'localhost'; // host server
-	$user 	= 'id21084691_tokohmuhudin';  // username server
-	$pass 	= 'kasirH.muhidin'; // password server, kalau pakai xampp kosongin saja
-	$dbname = 'id21084691_kasirr'; // nama database anda
+	$user 	= 'root';  // username server
+	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
+	$dbname = 'kasirr'; // nama database anda
 	
 	try{
 		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
